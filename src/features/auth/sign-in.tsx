3cbe1cc3e00c983423a42658fd53a1a879fea9b1
jamie 🌨️ <jamie@wisp.bio>
2024-12-29 01:43:01 -0500

    feat: :sparkles: add better-auth link to sign in page footer

--- src/features/auth/sign-in.tsx
+++ src/features/auth/sign-in.tsx
@@ -4,17 +4,25 @@ import {
   Card,
   CardContent,
   CardDescription,
+  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
 import Icons from "@/components/ui/icons";
 import SignInButton from "@/components/ui/sign-in-button";
+import Link from "next/link";
+import { Link as TransitionLink } from "next-view-transitions";
 
 export default function SignIn() {
   return (
     <div className="grid w-full grow items-center px-4 sm:justify-center">
       <div className="motion-preset-fade flex flex-col gap-4">
-        <p className="text-center font-serif text-4xl font-bold">gloss</p>
+        <TransitionLink
+          href="/"
+          className="text-center font-serif text-4xl font-bold underline-offset-4 hover:underline"
+        >
+          gloss
+        </TransitionLink>
         <Card className="w-[24vw]">
           <CardHeader>
             <CardTitle className="text-center font-serif text-2xl font-bold">
@@ -24,7 +32,7 @@ export default function SignIn() {
               Sign in with the following providers
             </CardDescription>
           </CardHeader>
-          <CardContent className="flex flex-col gap-2">
+          <CardContent className="flex flex-col gap-2 py-0 pb-2">
             <SignInButton provider="github">
               <Icons.GitHub />
               GitHub
@@ -33,6 +41,22 @@ export default function SignIn() {
               <Icons.Discord />
               Discord
             </SignInButton>
+            <CardFooter className="px-6 pb-0 pt-2">
+              <div className="flex w-full justify-center border-t">
+                <p className="py-4 text-center text-xs text-neutral-500">
+                  Powered by{" "}
+                  <Link
+                    href="https://better-auth.com"
+                    className="underline"
+                    target="_blank"
+                  >
+                    <span className="dark:text-orange-200/90">
+                      better-auth.
+                    </span>
+                  </Link>
+                </p>
+              </div>
+            </CardFooter>
           </CardContent>
         </Card>
         <Button
