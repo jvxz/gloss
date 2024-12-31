0582072abaed104e275faa75dab5dd23dd8de63d
jamie 🌨️ <jamie@wisp.bio>
2024-12-31 01:40:26 -0500

    fix: :bug: fix middleware redirect bug

--- src/middleware.ts
+++ src/middleware.ts
@@ -22,7 +22,7 @@ export default async function authMiddleware(request: NextRequest) {
         },
     );
 
-    if (session) {
+    if (session && request.nextUrl.pathname !== "/privacy") {
         return NextResponse.redirect(new URL("/", request.url));
     }
     return NextResponse.next();
