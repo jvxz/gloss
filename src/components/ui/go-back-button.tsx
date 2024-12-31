78da93d273e7727217f69a87479210ab7c883528
jamie 🌨️ <jamie@wisp.bio>
2024-12-30 19:48:06 -0500

    edit go back button

--- src/components/ui/go-back-button.tsx
+++ src/components/ui/go-back-button.tsx
@@ -1,10 +1,18 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { ChevronLeft } from "lucide-react";
 import { Link } from "next-view-transitions";
+import { useRouter } from "next/navigation";
 
 export default function GoBackButton() {
+  const router = useRouter();
   return (
-    <Button asChild className="text-md group mx-0 mb-4 px-0" variant="link">
+    <Button
+      asChild
+      className="text-md group mx-0 mb-4 px-0"
+      variant="link"
+      onClick={() => router.back()}
+    >
       <Link href="/auth/sign-in">
         <ChevronLeft
           className="!size-5 opacity-60 transition-opacity group-hover:opacity-100"
